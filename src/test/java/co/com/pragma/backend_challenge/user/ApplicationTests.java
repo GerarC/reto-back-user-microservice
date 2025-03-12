d4bc104959180222297772bff78c061fa6e3879e
Gerardo Castillo <gerardo.castillo@pragma.com.co>
2025-03-12 16:45:54 -0500

    initial commit

---
+++ src/test/java/co/com/pragma/backend_challenge/user/ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.pragma.backend_challenge.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
